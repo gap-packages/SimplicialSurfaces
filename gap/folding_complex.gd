9197432309646723874786b9cb2faf636f172730
Markus Baumeister <markus.baumeister1@rwth-aachen.de>
2017-01-01 16:02:19 +0100

    Add licence to end of files.

--- gap/folding_complex.gd
+++ gap/folding_complex.gd
@@ -1,6 +1,6 @@
 ##############################################################################
 ##
-#W  folding_complex.gd          Simplicial             Markus Baumeister
+#W  folding_complex.gd          SimplicialSurfaces           Markus Baumeister
 ##
 ##
 #Y  Copyright (C) 2016-2017, Markus Baumeister, Lehrstuhl B für Mathematik,
@@ -9,7 +9,7 @@
 ##  This file is free software, see license information at the end.
 ##
 ##  This file contains the declaration part for the folding complexes
-##	of the Simplicial package.
+##	of the SimplicialSurfaces package.
 ##
 
 FoldingComplexFamily := 
@@ -18,3 +18,19 @@ FoldingComplexFamily :=
 DeclareCategory( "IsFoldingComplex", IsComponentObjectRep );
 
 
+
+
+#
+###  This program is free software: you can redistribute it and/or modify
+###  it under the terms of the GNU General Public License as published by
+###  the Free Software Foundation, either version 3 of the License, or
+###  (at your option) any later version.
+###
+###  This program is distributed in the hope that it will be useful,
+###  but WITHOUT ANY WARRANTY; without even the implied warranty of
+###  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+###  GNU General Public License for more details.
+###
+###  You should have received a copy of the GNU General Public License
+###  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###
--- gap/folding_complex.gi
+++ gap/folding_complex.gi
@@ -1,6 +1,6 @@
 ##############################################################################
 ##
-#W  folding_complex.gi          Simplicial         Markus Baumeister
+#W  folding_complex.gi          SimplicialSurfaces         Markus Baumeister
 ##
 ##
 #Y  Copyright (C) 2016-2017, Markus Baumeister, Lehrstuhl B für Mathematik,
@@ -19,3 +19,22 @@ DeclareRepresentation("FoldingComplexRep", IsSimplicialSurface,
 # for any list re
 FoldingComplexType := 
     NewType( FoldingComplexFamily, IsFoldingComplex );
+
+
+
+
+
+#
+###  This program is free software: you can redistribute it and/or modify
+###  it under the terms of the GNU General Public License as published by
+###  the Free Software Foundation, either version 3 of the License, or
+###  (at your option) any later version.
+###
+###  This program is distributed in the hope that it will be useful,
+###  but WITHOUT ANY WARRANTY; without even the implied warranty of
+###  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+###  GNU General Public License for more details.
+###
+###  You should have received a copy of the GNU General Public License
+###  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###
