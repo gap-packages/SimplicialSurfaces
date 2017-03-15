43a084d8f4d6655c314ea46fe21aa248dfd2c3f7
Markus Baumeister <markus.baumeister1@rwth-aachen.de>
2017-03-15 17:11:13 +0100

    Write introduction to documentation about simplicial surfaces

--- gap/simplicial_surface.gd
+++ gap/simplicial_surface.gd
@@ -12,28 +12,50 @@
 ##  This file contains the declaration part for the simplicial surfaces
 ##	of the SimplicialSurfaces package.
 ##
-##	A SimplicialSurface consists of vertices, edges and faces. Each of those
-##	is enumerated by positive integers (not necessarily consecutively).
-##	Furthermore we have an incidence relation between them.
-##
-##	It is necessary that each vertex is incident to at least one edge and one
-##	face. The same restriction also holds for edges and faces.
-##
-##	We additionally require a local orientation for each face - that is we
-##	want to distinguish its two sides. Therefore we also need names for both
-##	sides. The default naming scheme (which will be applied if the user does
-##	not explicitly objects) is that the two sides of the face f will be called
-##	+f and -f. With this scheme all positive integers represent the "upper
-##	sides" and negative integers the "lower sides". A side effect is that the
-##	name of a face and of its upper side coincide. It is usually clear from
-##	context which one is meant.
-##	Mathematically the local orientation is an cycle of the vertices inside
-##	a face such that two adjacent vertices in the cycle are also adjacent in
-##	the face.
-##
-##
 
+#!
 #!	@Chapter Simplicial Surfaces
+#!
+#!  A SimplicialSurface-object in GAP represents mathematical objects that are
+#!  a generalization of simplicial surfaces. On the most basic level it
+#!  consists of vertices, edges and faces, together with an incidence relation
+#!  between them. This information is saved in the following form:
+#!  * the vertices are represented by a set of positive integers
+#!  * the edges are represented by a set of positive integers
+#!  * the faces are represented by a set of positive integers
+#!  * the incidence relation between vertices and edges (that is, which
+#!      vertices lie in which edges) is represented by a list VerticesOfEdges.
+#!      For each edge e the entry VerticesOfEdges[e] is a set of all vertices
+#!      that are incident to the edge e.
+#!  * the incidence relation between edges and faces (that is, which
+#!      edges lie in which faces) is represented by a list EdgesOfFaces. For
+#!      each face f the entry EdgesOfFaces[f] is a set of all edges that are
+#!      incident to the face f.
+#!  Every other incidence (like VerticesOfFaces or EdgesOfVertices) is 
+#!  represented in an analogous fashion.
+#!
+#!  Furthermore we impose some restrictions onto these incidence relations:
+#!  * Every edge is incident to exactly two vertices
+#!  * Every face is incident to the same number of vertices and edges (at least
+#!      three). Additionally these are cyclically oriented (to represent an 
+#!      n-gon)
+#!  * The incidence relations are transitive
+#!  * Every vertex and every edge is incident to at least one face
+#!
+#!  Note that it is allowed for an edge to be incident to more than two faces.
+#!
+#!  
+#!  Furthermore it is sometimes necessary to distinguish between the two
+#!  sides of a single face. If this is irrelevant to your application, you
+#!  can completely ignore this (it will be handled in the background).
+#!  To use this distinction each side of a face f gets a name - the defaults
+#!  are +f and -f (but you can give them custom names if you want to). To 
+#!  distinguish which side is which (not only by a name, but geometrically)
+#!  we save a cyclic ordering of the vertices (or edges) of each face which
+#!  we associate with the side +f. (In an embedding to $\mathbb{R}^3$ we could
+#!  associate the cyclic ordering with the normal vector of the face that is
+#!  defined by the right-hand-rule from physics.)
+
 
 DeclareInfoClass( "InfoSimplicial" );
 SetInfoLevel(InfoSimplicial,1);
