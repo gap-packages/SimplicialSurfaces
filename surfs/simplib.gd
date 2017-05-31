5b12ac2ea70e47e4b5f60ba7882f2be2f34f7bd7
Alice Niemeyer <alice.niemeyer@mathb.rwth-aachen.de>
2017-05-31 15:53:01 +0200

    First files for surface library

---
+++ surfs/simplib.gd
@@ -0,0 +1,17 @@
+#############################################################################
+##
+#W  simpliciallib.gi       GAP simplicial surface library  
+##                                                         Markus Baumeister
+##                                                         Alice Niemeyer
+##                                                         Wilhelm Plesken
+##                                                         Ansgar Strzelczyk
+##
+##
+#Y  Copyright (C)  2017,  Lehrstuhl B für Mathematik, RWTH Aachen University 
+##
+##  This file contains the routines for the simplicial surfaces library
+##
+
+DeclareGlobalFunction( "NrSimplicialSurfaces" );
+DeclareGlobalFunction( "SimplicialSurfaceLib" );
+
