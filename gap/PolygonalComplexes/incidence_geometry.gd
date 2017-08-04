47ddce7a2da59e0be8b84300795a4534ac921088
Markus Baumeister <markus.baumeister1@rwth-aachen.de>
2017-08-04 14:47:59 +0200

    Add vertex documentation

--- gap/PolygonalComplexes/incidence_geometry.gd
+++ gap/PolygonalComplexes/incidence_geometry.gd
@@ -16,4 +16,20 @@
 #! @Section Basic access to the incidence structure
 #! @SectionLabel Access_BasicAccess
 #!
+#! In polygonal complexes (the most general supported incidence structure,
+#! compare chapter <Ref Chap="PolygonalStructures"/>) all vertices, edges
+#! and faces are labelled by positive integers. We can access the set of
+#! all those labels by <K>Vertices</K>, <K>Edges</K> and <K>Faces</K>.
+
+
+#! @BeginGroup
+## TODO check if grape has changed their definition of Vertices
+#! @Description
+#! Return the set of vertices.
+#! @Arguments complex
+#! @Returns A set of positive integers
+DeclareOperation( "Vertices", [IsPolygonalComplex] );
+#! @Arguments complex
+DeclareAttribute( "VerticesAttributeOfPolygonalComplex", IsPolygonalComplex );
+#! @EndGroup
 
