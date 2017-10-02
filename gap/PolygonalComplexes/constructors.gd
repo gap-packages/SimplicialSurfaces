8b254f4ae212a581952b665504b4575ceba8746f
Markus Baumeister <markus.baumeister1@rwth-aachen.de>
2017-10-02 10:58:04 +0200

    Write first paragraph of constructors chapter

--- gap/PolygonalComplexes/constructors.gd
+++ gap/PolygonalComplexes/constructors.gd
@@ -10,4 +10,15 @@
 ##
 #############################################################################
 
+#! @Chapter Constructing surfaces
+#! @ChapterLabel Constructors
+#!
+#! In chapter <Ref Chap="PolygonalStructures"/> we introduced polygonal
+#! complexes (as a generalization of simplicial surfaces). They are based
+#! on incidence structures, which were analysed in chapter
+#! <Ref Chap="Chapter_AccessIncidenceGeometry"/>.
+#! <Par/>
+#! This chapter deals with the construction of custom polygonal complexes
+#! and simplicial surfaces.
 
+#TODO which order of the sections?
