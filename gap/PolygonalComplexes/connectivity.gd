0e31ace4f9f13a3df59ea8ce69eeca17d8d0df8c
Markus Baumeister <markus.baumeister1@rwth-aachen.de>
2017-10-12 16:54:05 +0200

    Add more dummy files for later code

---
+++ gap/PolygonalComplexes/connectivity.gd
@@ -0,0 +1,11 @@
+#############################################################################
+##
+##  SimplicialSurface package
+##
+##  Copyright 2012-2016
+##    Markus Baumeister, RWTH Aachen University
+##    Alice Niemeyer, RWTH Aachen University 
+##
+## Licensed under the GPL 3 or later.
+##
+#############################################################################
