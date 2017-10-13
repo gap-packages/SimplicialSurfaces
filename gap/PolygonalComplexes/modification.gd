c88ad9f57dbcd246ab36b3b934f315102f129dce
Markus Baumeister <markus.baumeister1@rwth-aachen.de>
2017-10-13 23:21:31 +0200

    Sketch contents of modifications chapter

--- gap/PolygonalComplexes/modification.gd
+++ gap/PolygonalComplexes/modification.gd
@@ -9,3 +9,21 @@
 ## Licensed under the GPL 3 or later.
 ##
 #############################################################################
+
+#TODO duality where combining and cutting method are described together?
+
+# disjoint union <-> Subsurface
+
+
+# Connected sum over
+# * face
+# * edge (where edge blows up)
+# <-> Separation at two-waist or three-waist
+
+# cut/mend edges
+
+# These do not fit the above pattern:
+# SnippOffEars
+# CommonCover
+# DualSurface
+# AddVertexIntoEdge (and the rest of Jesse's stuf)
