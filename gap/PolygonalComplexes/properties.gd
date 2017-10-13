ad713a2043026dc77d0947464e004be831d203ff
Markus Baumeister <markus.baumeister1@rwth-aachen.de>
2017-10-13 16:31:41 +0200

    Add vertex and edge counter

--- gap/PolygonalComplexes/properties.gd
+++ gap/PolygonalComplexes/properties.gd
@@ -33,9 +33,8 @@
 #!
 #! TODO
 
-#TODO is an example needed? Does the code speak for its own?
 #! @Description
-#! Return the Euler-characteristic of the given polygonal complex.
+#! Return the <E>Euler-characteristic</E> of the given polygonal complex.
 #! The Euler-characteristic is computed as
 #! @BeginLogSession
 #! gap> NrOfVertices(complex) - NrOfEdges(complex) + NrOfFaces(complex);
@@ -62,3 +61,54 @@
 #! @Returns an integer
 #! @Arguments complex
 DeclareAttribute( "EulerCharacteristic", IsPolygonalComplex );
+
+
+#! @Description
+#! Return the <E>vertex counter</E> of the given polygonal complex.
+#! The vertex counter is a list that counts how many vertices are incident
+#! to how many edges. If the entry at position <A>pos</A> is bound, it
+#! contains the number of vertices that are incident to exactly <A>pos</A>
+#! edges.
+#!
+#! As an example, consider the following simplicial surface:
+#! <Alt Only="TikZ">
+#!   \begin{tikzpicture}[vertexStyle, edgeStyle=nolabels, faceStyle]
+#!      \input{Image_FiveTrianglesInCycle.tex};
+#!   \end{tikzpicture}
+#! </Alt>
+#! @ExampleSession
+#! gap> complex := SimplicialSurfaceByVerticesInFaces( 6, 5, 
+#! >                [ [1,2,3], [1,3,4], [1,4,5], [1,5,6], [1,2,6] ] );;
+#! gap> List( EdgesOfVertices(complex), Size );
+#! [ 5, 3, 3, 3, 3, 3 ]
+#! gap> VertexCounter(complex);
+#! [ , , 5, , 1 ]
+#! @EndExampleSession
+#!
+#! @Returns a list of positive integers
+#! @Arguments complex
+DeclareAttribute( "VertexCounter", IsPolygonalComplex );
+
+
+#! @Description
+#! Return the <E>edge counter</E> of the given polygonal complex.
+#! The edge counter is a symmetric matrix <M>M</M> such that the entry
+#! <M>M[i,j]</M> counts the number of edges such that the two vertices
+#! of the edge are respectively incident to <M>i</M> and <M>j</M> edges.
+#!
+#! As an example, consider the following simplicial surface:
+#! <Alt Only="TikZ">
+#!   \begin{tikzpicture}[vertexStyle, edgeStyle=nolabels, faceStyle]
+#!      \input{Image_FiveTrianglesInCycle.tex};
+#!   \end{tikzpicture}
+#! </Alt>
+#! @ExampleSession
+#! gap> complex := SimplicialSurfaceByVerticesInFaces( 6, 5, 
+#! >                [ [1,2,3], [1,3,4], [1,4,5], [1,5,6], [1,2,6] ] );;
+#! gap> EdgeCounter(complex);
+#! [ [ , , , , ], [ , , , , ], [ , , 5, , 5 ], [ , , , , ], [ , , 5, , ] ]
+#! @EndExampleSession
+#!
+#! @Returns a matrix of positive integers
+#! @Arguments complex
+DeclareAttribute( "EdgeCounter", IsPolygonalComplex );
--- gap/PolygonalComplexes/properties.gi
+++ gap/PolygonalComplexes/properties.gi
@@ -22,6 +22,70 @@ InstallMethod( EulerCharacteristic, "for a polygonal complex",
     end
 );
 
+InstallMethod( VertexCounter, "for a polygonal complex",
+    [IsPolygonalComplex],
+    function(complex)
+        local edgeDegrees, maxDeg, sym, d, pos;
+
+        edgeDegrees := List( EdgesOfVertices(complex), Size );
+        maxDeg := Maximum(edgeDegrees);
+        sym := [];
+        for d in [1..maxDeg] do
+            pos := Size( Positions(edgeDegrees, d) );
+            if pos <> 0 then
+                sym[d] := pos;
+            fi;
+        od;
+
+        return sym;
+    end
+);
+InstallMethod( VertexCounter, "for a polygonal complex with computed EdgeCounter",
+    [IsPolygonalComplex and HasEdgeCounter],
+    function(complex)
+        local symbol, i, j, edgeCounter, sum;
+
+        symbol := [];
+        edgeCounter := EdgeCounter(surf);
+        for i in [1..Size(edgeCounter)] do
+            sum := edgeCounter[i][i];   # This is counted twice
+            for j in [1..Size(edgeCounter)] do
+                sum := sum + edgeCounter[i][j];
+            od;
+            sum := sum/i;
+            if sum <> 0 then
+                symbol[i] := sum;
+            fi;
+        od;
+
+        return symbol;
+    end
+);
+
+InstallMethod( EdgeCounter, "for a polygonal complex",
+    [IsPolygonalComplex],
+    function(complex)
+        local edgeDegrees, max, edge, symbol, degs;
+
+        edgeDegrees := List( EdgesOfVertices(simpSurf), Size );
+        if NrOfEdges(simpSurf) = 0 then
+            return [];
+        fi;
+        max := Maximum( edgeDegrees ); # bigger than zero since edges exist
+
+        # Set up the matrix
+        symbol := List( [1..max], i -> List( [1..max], j -> 0 ) );
+        for edge in Edges(simpSurf) do
+            degs := List( VerticesOfEdges(simpSurf)[edge], v -> edgeDegrees[v] );
+            symbol[ degs[1] ][ degs[2] ] := symbol[degs[1]][degs[2]] + 1;
+            if degs[1] <> degs[2] then
+                symbol[ degs[2] ][ degs[1] ] := symbol[degs[2]][degs[1]] + 1;
+            fi;
+        od;
+
+        return symbol;
+    end
+);
 
 ##
 ##      End of invariants
