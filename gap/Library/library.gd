07fc5e87010895f82fc9dacc78c32805212d0c3d
Markus Baumeister <markus.baumeister1@rwth-aachen.de>
2018-02-01 13:22:00 +0100

    Framework for library access

---
+++ gap/Library/library.gd
@@ -0,0 +1,16 @@
+#############################################################################
+##
+##  SimplicialSurface package
+##
+##  Copyright 2012-2018
+##    Markus Baumeister, RWTH Aachen University
+##    Alice Niemeyer, RWTH Aachen University 
+##
+## Licensed under the GPL 3 or later.
+##
+#############################################################################
+
+#! @Chapter Library of surfaces
+#! @ChapterLabel Library
+#! 
+#!
