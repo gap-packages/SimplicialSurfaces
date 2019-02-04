78b0c01130ee2a105b2e2f65381f8c56b6bfbd58
Markus Baumeister <markus.baumeister1@rwth-aachen.de>
2019-02-04 14:22:07 +0100

    Write down the requirements to implement the morphisms fully.

--- gap/Morphisms/morphisms.gd
+++ gap/Morphisms/morphisms.gd
@@ -19,6 +19,8 @@
 #!
 #! TODO a lot of introduction
 
+#idea: polygonal morphisms are maps between sets: vertices+edges+faces, where edges
+# are shifted by MaxVertex and faces are shifted by MaxVertex+MaxEdge
 DeclareCategory("IsGeneralPolygonalMorphism", IsNonSPGeneralMapping);
 # TODO maybe add more specific properties, like IsPolygonalMorphism
 # TODO are the names good? Or should we be more specific, e.g. IsPolygonalComplexMorphism, IsPolygonalSurfaceMorphism?
@@ -27,3 +29,43 @@ DeclareCategory("IsGeneralPolygonalMorphism", IsNonSPGeneralMapping);
 BindGlobal( "GeneralPolygonalMorphismFamily", 
     NewFamily( "GeneralPolygonalMorphismFamily", IsGeneralMapping, IsGeneralPolygonalMorphism ) );
 
+
+#List of TODO:
+
+# For a polygonal complex: maps between edges and shiftedEdges (compare graph.gi);
+# For a polygonal complex: maps between faces and shiftedFaces (compare graph.gi);
+
+# attribute SourceComplex
+# attribute SourceSurface
+# attribute RangeComplex
+# attribute RangeSurface
+
+# overload CompositionMapping2
+
+# attribute VertexMapping
+# attribute EdgeMapping
+# attribute FaceMapping
+
+# implement PreImagesOfVertex
+# implement PreImagesOfEdge
+# implement PreImagesOfFace
+
+# implement ImagesOfVertex
+# implement ImagesOfEdge
+# implement ImagesOfFace
+
+# implement ImageOfVertex (for a total and single-valued ..);
+# implement ImageOfEdge (for a total and single-valued ..);
+# implement ImageOfFace (for a total and single-valued ..);
+
+# implement IdentityMapping
+
+# document some of the generic properties like IsInjective, IsSurjective, ...
+
+# implement InverseMapping/InverseMorphism (for appropriate...)
+
+# implement constructor by three lists (images of vertices, edges and faces)
+
+# implement VertexMapAsImageList (for single-valued);
+# implement EdgeMapAsImageList (for single-valued);
+# implement FaceMapAsImageList (for single-valued);
