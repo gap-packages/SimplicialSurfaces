f36a5f65327eb87938a9940b1850c6430578c585
Markus Baumeister <markus.baumeister1@rwth-aachen.de>
2019-08-27 15:35:36 +0200

    Add all discs of type [6,6,0,10,*]

--- gap/Library/library.gd
+++ gap/Library/library.gd
@@ -307,7 +307,7 @@ DeclareGlobalFunction("AllGeodesicSelfDualSurfaces");
 #! @BeginExampleSession
 #! gap> discs := AllDiscs();;
 #! gap> Length(discs);
-#! 37
+#! 41
 #! gap> AllDiscs(function(surf) return 7 in FaceDegreesOfVertices(surf); end);
 #! []
 #! gap> List(discs, UmbrellaPathsOfVertices);;
