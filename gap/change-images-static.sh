7c320a029413a0a4c9aa617bfc8c9104f5189b0e
Lukas Schnelle <lukas.schnelle1@rwth-aachen.de>
2023-05-13 06:41:20 +0200

    add working files for changing to static images

--- gap/change-images-static.sh
+++ gap/change-images-static.sh
@@ -4,7 +4,6 @@ for gdfile in $(find  -type f -name "*.gd");
 do
     echo $gdfile 
     cp $gdfile $gdfile.orig
-#    cat $gdfile.orig | tr '\r\n' '%' | sed --debug -f sed-images-static | tr '%' '\r\n'  > $gdfile.changed
-    cat $gdfile.orig | tr -s '\r\n' '%' | sed --debug -f sed-images-static > $gdfile.changed
+    cat $gdfile.orig | tr '\n' '%' | sed -f sedin | tr '%' '\n'  > $gdfile
     rm $gdfile.orig
 done;
