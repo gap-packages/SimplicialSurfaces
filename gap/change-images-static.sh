19f9ecbd912a176fbe48c5762cf5c25704100c08
Lukas Schnelle <schnelle.com@gmail.com>
2023-07-03 23:13:28 +0200

    remove scripts used to clean up

--- gap/change-images-static.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for gdfile in $(find  -type f -name "*.gd");
-do
-    echo $gdfile 
-    cp $gdfile $gdfile.orig
-    cat $gdfile.orig | tr '\n' '%' | sed -f sedin | tr '%' '\n'  > $gdfile
-    rm $gdfile.orig
-done;
--- gap/images2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for gdfile in $(find  -type f -name "*.gd");
-do
-    echo $gdfile 
-    cp $gdfile $gdfile.orig
-    cat $gdfile.orig | tr '\n' '%' | sed --debug -f sedin2 | tr '%' '\n'  > $gdfile.debug
-    rm $gdfile.orig
-done;
