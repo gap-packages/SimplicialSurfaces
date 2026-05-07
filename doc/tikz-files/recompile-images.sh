93ca49e21f085afdd697d19d322c982c4d0d1791
Max Horn <max@quendi.de>
2026-05-07 15:22:25 +0100

    another tweak

--- doc/tikz-files/recompile-images.sh
+++ doc/tikz-files/recompile-images.sh
@@ -6,28 +6,19 @@ export FORCE_SOURCE_DATE=1
 export LC_ALL=C
 export TZ=UTC
 
-find . -type f -name '_Wrapper_*.tex' -print0 |
-  parallel -0 --bar '
-    dir=$(dirname "{}")
-    base=$(basename "{}")
-    filename="${base%.tex}"
-    
-    (
-      cd "$dir" || exit 1
-      
-      mkdir -p log
-      logfile="log/${base}.log"
-      
-      # Run pdflatex silently; if it fails, print the clean error and exit
-      if ! pdflatex -halt-on-error -interaction=nonstopmode -output-directory ../images "$base" > "$logfile" 2>&1; then
-        echo -e "\nError compiling: $base -> Logs at: $dir/$logfile"
+parallel --bar '
+    mkdir -p log
+    logfile="log/{}.log"
+
+    # Run pdflatex silently; if it fails, print the clean error and exit
+    if ! pdflatex -halt-on-error -interaction=nonstopmode -output-directory ../images {} > "$logfile" 2>&1; then
+        echo -e "\nError compiling: {} -> Logs at: $logfile"
         exit 1
-      fi
-      
-      # Run pdftocairo silently; if it fails, print the clean error and exit
-      if ! pdftocairo -svg "../images/${filename}.pdf" "../images/${filename}-1.svg" >> "$logfile" 2>&1; then
-        echo -e "\nError converting: $base -> Logs at: $dir/$logfile"
+    fi
+
+    # Run pdftocairo silently; if it fails, print the clean error and exit
+    if ! pdftocairo -svg "../images/{.}.pdf" "../images/{.}-1.svg" >> "$logfile" 2>&1; then
+        echo -e "\nError converting: {} -> Logs at: $logfile"
         exit 1
-      fi
-    )
-  '
+    fi
+' ::: _Wrapper_*.tex
