cb86979d570a1c5e6b77afa470c369c6ba3fbf36
Lukas Schnelle <schnelle.com@gmail.com>
2026-05-07 15:33:51 +0200

    get every pdf and svg file to be reproducible, to minimise git history

--- doc/tikz-files/recompile-images.sh
+++ doc/tikz-files/recompile-images.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
-for texfile in $(find . -type f -name "_Wrapper_*.tex");
-do
-    # cp $gdfile $gdfile.orig
-    # cat $gdfile.orig | tr '\n' '%' | sed -f sedin | tr '%' '\n'  > $gdfile
-    # rm $gdfile.orig
-    pdflatex -halt-on-error -output-directory ../images $texfile
-    htlatex $texfile
-done;
-mv *.svg ../images
+# set some flags for reproducibility
+export SOURCE_DATE_EPOCH=1
+export FORCE_SOURCE_DATE=1
+export LC_ALL=C
+export TZ=UTC
+
+find . -type f -name '_Wrapper_*.tex' -print0 |
+  parallel -0 --bar '
+    dir=$(dirname "{}")
+    base=$(basename "{}")
+    filename="${base%.tex}"
+    
+    (
+      cd "$dir" || exit 1
+      
+      mkdir -p log
+      logfile="log/${base}.log"
+      
+      # Run pdflatex silently; if it fails, print the clean error and exit
+      if ! pdflatex -halt-on-error -interaction=nonstopmode -output-directory ../images "$base" > "$logfile" 2>&1; then
+        echo -e "\nError compiling: $base -> Logs at: $dir/$logfile"
+        exit 1
+      fi
+      
+      # Run pdftocairo silently; if it fails, print the clean error and exit
+      if ! pdftocairo -svg "../images/${filename}.pdf" "../images/${filename}-1.svg" >> "$logfile" 2>&1; then
+        echo -e "\nError converting: $base -> Logs at: $dir/$logfile"
+        exit 1
+      fi
+    )
+  '
